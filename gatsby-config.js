526bca8c7e1f6088bd98213ab3b8f587b96300eb
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-10-28 02:07:29 +0000

    React 17. Fixing gtag.

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,14 +34,26 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-GTN7TJT5W3",
-        head: true,
-        // TODO: find out if this is required due to any privacy laws
-        // anonymize: true,
-        // respectDNT: true,
-        pageTransitionDelay: 0,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-GTN7TJT5W3"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     `gatsby-plugin-react-helmet`,
